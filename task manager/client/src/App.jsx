1adb622b49abe70548b50710d6b0083c73d6967a
ayushrai1235 <prathibharai64@gmail.com>
2025-01-10 23:52:29 +0530

    Routing structure and layout

--- task manager/client/src/App.jsx
+++ task manager/client/src/App.jsx
@@ -1,50 +1,64 @@
-import Login from './pages/Login.jsx'
-import { Routes, Route, Navigate, Outlet, useLocation, replace} from 'react-router-dom'
-import Dashboard from './pages/dashboard.jsx'
-import Tasks from './pages/tasks.jsx'
-import Users from './pages/Users.jsx'
-import Trash from './pages/Trash.jsx'
-import TaskDetails from './pages/TaskDetails.jsx'
-import { Toaster } from 'sonner'
+import Login from "./pages/Login.jsx";
+import {
+  Routes,
+  Route,
+  Navigate,
+  Outlet,
+  useLocation,
+  replace,
+} from "react-router-dom";
+import Dashboard from "./pages/dashboard.jsx";
+import Tasks from "./pages/tasks.jsx";
+import Users from "./pages/Users.jsx";
+import Trash from "./pages/Trash.jsx";
+import TaskDetails from "./pages/TaskDetails.jsx";
+import { Toaster } from "sonner";
 
 function Layout() {
-  const user = ""
-  const location = useLocation()
+  const user = "";
+  const location = useLocation();
   return user ? (
-    <div className='w-full h-screen flex flex-col md:flex-row'>
-        <div className='w-1/5 h-screen bg-white sticky top-0 hidden md:block'>
-          {/* <Sidebar /> */}
+    <div className="w-full h-screen flex flex-col md:flex-row">
+      <div className="w-1/5 h-screen bg-white sticky top-0 hidden md:block">
+        {/* <Sidebar /> */}
+      </div>
+
+      {/* <MobileSidebar /> */}
+
+      <div className="flex-1 overflow-y-auto">
+        {/* <Navbar/> */}
+
+        <div className="p-4 2xl:px-10">
+          <Outlet />
         </div>
-
-        {/* <MobileSidebar /> */}
-
+      </div>
     </div>
-  ):(
-    <Navigate to='/login' state={{from: location}} replace/>
-  )
+  ) : (
+    <Navigate to="/login" state={{ from: location }} replace />
+  );
 }
 function App() {
   return (
-    <main className='w-full min-h-screen bg-[#f3f4f6]'>
-    <Routes>
-      <Route element={<Layout/>}>
-        <Route path='/' element={<Navigate to='/dashboard'/>} />
-        <Route path='/dashboard >' element= {<Dashboard/>}/>
-        <Route path='/tasks >' element= {<Tasks />}/>
-        <Route path='/completed/:status >' element= {<Tasks/>}/>
-        <Route path='/in-progress/:status >' element= {<Tasks/>}/>
-        <Route path='/todo/:status >' element= {<Tasks/>}/>
-        <Route path='/teams >' element= {<Users />}/>
-        <Route path='/trashed >' element= {<Trash />}/>
-        <Route path='/task/:id >' element= {<TaskDetails />}/>
-      </Route>
+    <main className="w-full min-h-screen bg-[#f3f4f6]">
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<Navigate to="/dashboard" />} />
+          <Route path="/dashboard >" element={<Dashboard />} />
+          <Route path="/tasks >" element={<Tasks />} />
+          <Route path="/completed/:status >" element={<Tasks />} />
+          <Route path="/in-progress/:status >" element={<Tasks />} />
+          <Route path="/todo/:status >" element={<Tasks />} />
+          <Route path="/teams >" element={<Users />} />
+          <Route path="/trashed >" element={<Trash />} />
+          <Route path="/task/:id >" element={<TaskDetails />} />
+        </Route>
 
-      <Route path='/login' element= {<Login />}/>
-    </Routes>
+        <Route path="/login" element={<Login />} />
+      </Routes>
 
-    <Toaster richColors />
-  </main>
-  )
+      <Toaster richColors />
+    </main>
+  );
 }
 
-export default App
+export default App;
