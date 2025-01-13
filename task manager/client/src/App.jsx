01c2432ab1d6cd4369a2017a1fe2618977783c78
ayushrai1235 <prathibharai64@gmail.com>
2025-01-14 03:15:53 +0530

    Add Sidebar component and integrate it into the Layout

--- task manager/client/src/App.jsx
+++ task manager/client/src/App.jsx
@@ -13,14 +13,16 @@ import Trash from "./pages/Trash.jsx";
 import TaskDetails from "./pages/TaskDetails.jsx";
 import { Toaster } from "sonner";
 import { useSelector } from "react-redux";
+import Sidebar from "./components/Sidebar.jsx";
 
 function Layout() {
-  const {user} = useSelector(state => state.auth);
+  const user = true;
+  // const {user} = useSelector(state => state.auth);
   const location = useLocation();
   return user ? (
     <div className="w-full h-screen flex flex-col md:flex-row">
       <div className="w-1/5 h-screen bg-white sticky top-0 hidden md:block">
-        {/* <Sidebar /> */}
+        <Sidebar/>
       </div>
 
       {/* <MobileSidebar /> */}
@@ -43,14 +45,14 @@ function App() {
       <Routes>
         <Route element={<Layout />}>
           <Route index path="/" element={<Navigate to="/dashboard" />} />
-          <Route path="/dashboard " element={<Dashboard />} />
-          <Route path="/tasks " element={<Tasks />} />
-          <Route path="/completed/:status " element={<Tasks />} />
-          <Route path="/in-progress/:status " element={<Tasks />} />
-          <Route path="/todo/:status " element={<Tasks />} />
-          <Route path="/teams " element={<Users />} />
-          <Route path="/trashed " element={<Trash />} />
-          <Route path="/task/:id " element={<TaskDetails />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/tasks" element={<Tasks />} />
+          <Route path="/completed/:status" element={<Tasks />} />
+          <Route path="/in-progress/:status" element={<Tasks />} />
+          <Route path="/todo/:status" element={<Tasks />} />
+          <Route path="/teams" element={<Users />} />
+          <Route path="/trashed" element={<Trash />} />
+          <Route path="/task/:id" element={<TaskDetails />} />
         </Route>
 
         <Route path="/login" element={<Login />} />
