e23b428390779595d47dc9154c967fdc6ac0fdbe
ayushrai1235 <prathibharai64@gmail.com>
2025-01-16 23:01:22 +0530

    Implement mobile sidebar functionality and integrate toggle in Navbar

--- task manager/client/src/App.jsx
+++ task manager/client/src/App.jsx
@@ -15,6 +15,14 @@ import { Toaster } from "sonner";
 import { useSelector } from "react-redux";
 import Sidebar from "./components/Sidebar.jsx";
 import Navbar from "./components/Navbar.jsx";
+import { useDispatch } from "react-redux";
+import {toggleSidebar} from "./redux/slices/authSlice.js";
+import { useRef } from "react";
+import {Menu, Transition} from "@headlessui/react";
+import { Fragment } from "react";
+import clsx from "clsx";
+import { IoClose } from "react-icons/io5";
+import mobileMenuRef from "react";
 
 function Layout() {
   const user = true;
@@ -26,7 +34,7 @@ function Layout() {
         <Sidebar/>
       </div>
 
-      {/* <MobileSidebar /> */}
+      <MobileSidebar />
 
       <div className="flex-1 overflow-y-auto">
         <Navbar/>
@@ -40,6 +48,55 @@ function Layout() {
     <Navigate to="/login" state={{ from: location }} replace />
   );
 }
+
+const MobileSidebar = () => {
+  const { isSidebarOpen } = useSelector((state) => state.auth);
+  const mobileMenuref = useRef(null);
+  const dispatch = useDispatch();
+
+  const closeSidebar = () => {
+    dispatch(toggleSidebar(false));
+  }
+
+return (
+<>
+      <Transition
+        show={isSidebarOpen}
+        as={Fragment}
+        enter='transition-opacity duration-700'
+        enterFrom='opacity-x-10'
+        enterTo='opacity-x-100'
+        leave='transition-opacity duration-700'
+        leaveFrom='opacity-x-100'
+        leaveTo='opacity-x-0'
+      >
+        {(ref) => (
+          <div
+            ref={(node) => (mobileMenuref.current = node)}
+            className={clsx(
+              "md:hidden w-full h-full bg-black/40 transition-all duration-700 transform ",
+              isSidebarOpen ? "translate-x-0" : "translate-x-full"
+            )}
+            onClick={() => closeSidebar()}
+          >
+            <div className='bg-white w-3/4 h-full'>
+              <div className='w-full flex justify-end px-5 mt-5'>
+                <button
+                  onClick={() => closeSidebar()}
+                  className='flex justify-end items-end'
+                >
+                  <IoClose size={25} />
+                </button>
+              </div>
+
+              <div className='-mt-10'>
+                <Sidebar />
+              </div>
+            </div>
+          </div>
+        )}
+      </Transition>
+</>)}
 function App() {
   return (
     <main className="w-full min-h-screen bg-[#f3f4f6]">
--- task manager/client/src/components/Navbar.jsx
+++ task manager/client/src/components/Navbar.jsx
@@ -2,26 +2,27 @@ import React from "react";
 import { MdOutlineSearch } from "react-icons/md";
 import { useDispatch, useSelector } from "react-redux";
 import UserAvatar from "./UserAvatar.jsx";
+import { toggleSidebar } from "../redux/slices/authSlice.js";
 const Navbar = () => {
     const { user } = useSelector((state) => state.auth);
     const dispatch = useDispatch();
 
-    return <div className="flex justify-between items-center bg-white px-44 py-3 2xl:py-4 sticky z-10 top-0">
+    return (
+      <div className="flex justify-between items-center bg-white px-4 py-3 2xl:py-4 sticky z-10 top-0">
         <div className="flex gap-4">
         <button
-          onClick={() => dispatch(setOpenSidebar(true))}
+          onClick={() => dispatch(toggleSidebar(true))}
           className='text-2xl text-gray-500 block md:hidden'
         >
           ☰
         </button>
-        <div className="w-64 2xl:w-[400px] flex items-center py-2 px-3 gap-2 rounded-full bg-[#f3f4f6]">    
-            <MdOutlineSearch className="text-gray-500 text-xl"/>
-
+        <div className="w-64 2xl:w-[400px] flex items-center py-2 px-3 gap-2 rounded-full bg-[#f3f4f6]">
+          <MdOutlineSearch className='text-gray-500 text-xl' />
             <input type="text"
             placeholder="Search..." 
             className="flex-1 bg-transparent outline-none placeholder:text-gray-500 text-gray-800"
             />
-        </div>  -
+        </div>  
         </div>
 
         <div className="flex gap-2 items-center">
@@ -30,6 +31,8 @@ const Navbar = () => {
           <UserAvatar />
         </div>
     </div>
+    ) 
+    
 }
 
 export default Navbar
