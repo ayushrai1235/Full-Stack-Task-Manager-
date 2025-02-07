d60164a181a1b4b8deb7d1ae501746ad8205319e
ayushrai1235 <prathibharai64@gmail.com>
2025-02-08 00:06:51 +0530

    Add tasks reference and isActive field to user model; implement password hashing and matching methods

--- task manager/server/models/user.models.js
+++ task manager/server/models/user.models.js
@@ -8,6 +8,24 @@ const userSchema = new mongoose.Schema({
     email: {type: String, required: true, unique: true},
     password: {type: String, required: true},
     isAdmin: {type: Boolean, required: true, default: false},
+    tasks: [{type: mongoose.Schema.Types.ObjectId, ref: "Task"}],
+    isActive: {type: Boolean, required: true, default: true},
+}, {timestamps: true}
 
+);
 
+userSchema.pre("save", async function(next) {
+    if(!this.isModified("password")) {
+        next();
+    }
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
 })
+
+userSchema.method.matchPassword = async function(enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
+}
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
