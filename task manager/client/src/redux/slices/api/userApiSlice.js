037982524c7603bf0211bdd0cd6b53f75c5ebede
ayushrai1235 <prathibharai64@gmail.com>
2025-02-25 00:15:50 +0530

    Add user deletion and action handling in Users component; implement API mutations and toast notifications for user status updates

--- task manager/client/src/redux/slices/api/userApiSlice.js
+++ task manager/client/src/redux/slices/api/userApiSlice.js
@@ -20,7 +20,22 @@ export const userApiSlice = apiSlice.injectEndpoints({
                 credentials: "include",
             })
         }),
+        deleteUser: builder.mutation({
+            query: (id)=> ({
+                url: `${USER_URL}/${id}`,
+                method: "DELETE",
+                credentials: "include",
+            })
+        }),
+        userAction: builder.mutation({
+            query: (data)=> ({
+                url: `${USER_URL}/${data.id}`,
+                method: "PUT",
+                body: data,
+                credentials: "include",
+            })
+        }),
     })
 })
 
-export const { useUpdateUserMutation, useGetTeamListQuery} = userApiSlice
+export const { useUpdateUserMutation, useGetTeamListQuery, useDeleteUserMutation, useUserActionMutation} = userApiSlice
