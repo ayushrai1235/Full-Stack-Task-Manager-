193eedb150d4236d395f3d89454c98c99811b027
ayushrai1235 <prathibharai64@gmail.com>
2025-03-08 00:06:17 +0530

    Implement delete and restore functionality for tasks; enhance Trash component with loading state and error handling, and update API slice for deleteRestoreTask mutation

--- task manager/client/src/pages/Trash.jsx
+++ task manager/client/src/pages/Trash.jsx
@@ -13,6 +13,9 @@ import Button from "../components/Button";
 import { PRIOTITYSTYELS, TASK_TYPE } from "../utils/utils.js";
 import AddUser from "../components/AddUser";
 import ConfirmatioDialog from "../components/Dialogs";
+import { useDeleteRestoreTaskMutation, useGetAllTasksQuery } from "../redux/slices/api/taskApiSlice.js";
+import Loading from "../components/Loader.jsx";
+import { toast } from "sonner";
 
 const ICONS = {
   high: <MdKeyboardDoubleArrowUp />,
@@ -26,6 +29,49 @@ const Trash = () => {
   const [msg, setMsg] = useState(null);
   const [type, setType] = useState("delete");
   const [selected, setSelected] = useState("");
+
+  const {data, isLoading, refetch} = useGetAllTasksQuery({
+    strQuery: "",
+    isTrashed: "true",
+    search: "",
+  })
+  
+  const[deleteRestoreTask] = useDeleteRestoreTaskMutation()
+  
+  const deleteRestoreHandler = async () => {
+    try {
+      let result;
+
+      switch (type) {
+        case "delete":
+          result = await deleteRestoreTask({id: selected, actionType: "delete"}).unwrap()
+          break;
+        case "deleteAll":
+          result = await deleteRestoreTask({id: selected, actionType: "deleteAll"}).unwrap()
+          break;
+         
+        case "restore":
+          result = await deleteRestoreTask({id: selected, actionType: "restore"}).unwrap()
+          break;
+          
+        case "restoreAll":
+          result = await deleteRestoreTask({id: selected, actionType: "restoreAll"}).unwrap()
+          break;  
+
+      }
+
+      toast.success(result?.message);
+
+      setTimeout(() => {
+        setOpenDialog(false);
+        refetch();
+      
+      }, 500);
+    } catch (error) {
+      console.log(error)
+      toast.error(error?.data?.message || error.error)
+    }
+  }
 
   const deleteAllClick = () => {
     setType("deleteAll");
@@ -51,6 +97,13 @@ const Trash = () => {
     setMsg("Do you want to restore the selected item?");
     setOpenDialog(true);
   };
+
+  if (isLoading)
+    return(
+      <div className="py-10">
+        <Loading/>
+      </div>
+    );
 
   const TableHeader = () => (
     <thead className='border-b border-gray-300'>
@@ -129,7 +182,7 @@ const Trash = () => {
             <table className='w-full mb-5'>
               <TableHeader />
               <tbody>
-                {tasks?.map((tk, id) => (
+                {data?.tasks?.map((tk, id) => (
                   <TableRow key={id} item={tk} />
                 ))}
               </tbody>
